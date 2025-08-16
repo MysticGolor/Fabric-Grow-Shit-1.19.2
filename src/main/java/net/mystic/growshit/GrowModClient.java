e087afc9983ae6bf4778957cc2bf1b3d69e0c7d6
EM725 <edwardmckay2007@gmail.com>
2025-08-17 00:29:56 +0100

    Initial commit

---
+++ src/main/java/net/mystic/growshit/GrowModClient.java
@@ -0,0 +1,10 @@
+package net.mystic.growshit;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GrowModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
