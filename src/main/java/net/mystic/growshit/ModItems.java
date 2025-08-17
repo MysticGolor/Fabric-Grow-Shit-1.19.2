f7e7b6a4d19690c27f779a0878a3a451accba63c
EM725 <edwardmckay2007@gmail.com>
2025-08-17 01:33:40 +0100

    Got two custom items into the game however the textures aren't displaying properly along with the name

--- src/main/java/net/mystic/growshit/ModItems.java
+++ src/main/java/net/mystic/growshit/ModItems.java
@@ -1,17 +1,21 @@
 package net.mystic.growshit;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
-import net.mystic.growshit.GrowShit;
 import net.minecraft.item.Item;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
+    public static final Item UNCURED_BUD = registerItem("uncured_bud",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    public static final Item CURED_BUD = registerItem("cured_bud",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
 
 
     private static Item registerItem(String name, Item item){
-        return null;
-        //Registry.register(Registry.ITEM, new Identifier(GrowShit.MOD_ID, name), item);
+        return Registry.register(Registry.ITEM, new Identifier(GrowShit.MOD_ID, name), item);
     }
 
     public static void registerModItems(){
